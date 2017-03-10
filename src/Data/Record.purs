ef3258438975eaad5d72df22c45b480ae7140c0d
Jolse Maginnis <doolse@gmail.com>
2017-03-10 16:21:24 +1100

    Fix constraint for Left biased RowUnion

--- src/Data/Record.purs
+++ src/Data/Record.purs
@@ -2,4 +2,5 @@ module Data.Record where
 
 class RowUnion (l :: # *) (r :: # *) (o :: # *) | l r -> o
 
-foreign import merge :: forall a b c. RowUnion a b c => {|a} -> {|b} -> {|c}
+-- | Merge two records with the values in the second overriding the first
+foreign import merge :: forall a b c. RowUnion b a c => {|a} -> {|b} -> {|c}

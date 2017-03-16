4a286f61137f5875077581c3a89f04368277377d
Jolse Maginnis <doolse@gmail.com>
2017-03-17 08:52:10 +1100

    Use simpler normalised

--- src/Data/Record.purs
+++ src/Data/Record.purs
@@ -1,8 +1,8 @@
 module Data.Record where
 
-class RecordUnion (l :: # Type) (r :: # Type) (u :: # Type) (o :: # Type)
+class RecordUnion (l :: # Type) (r :: # Type) (u :: # Type) | l r -> u
 
-instance recordUnion :: (Union l r u, Normalised u o) => RecordUnion l r u o
+instance recordUnion :: (Union l r u1, Union r l u2, Normalised u1 u, Normalised u2 u) => RecordUnion l r u
 
 -- | Merge two records with the values in the second overriding the first
-foreign import merge :: forall a b u c. RecordUnion b a u c => {|a} -> {|b} -> {|c}
+foreign import merge :: forall a b u. RecordUnion b a u => {|a} -> {|b} -> {|u}

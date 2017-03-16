8666989f087228cf6b5e6a5f6a55191ee4234277
Jolse Maginnis <doolse@gmail.com>
2017-03-16 21:22:08 +1100

    PS 0.11 and RecordUnion

--- src/Data/Record.purs
+++ src/Data/Record.purs
@@ -1,6 +1,8 @@
 module Data.Record where
 
-class RowUnion (l :: # *) (r :: # *) (o :: # *) | l r -> o
+class RecordUnion (l :: # Type) (r :: # Type) (u :: # Type) (o :: # Type)
+
+instance recordUnion :: (Union l r u, Normalised u o) => RecordUnion l r u o
 
 -- | Merge two records with the values in the second overriding the first
-foreign import merge :: forall a b c. RowUnion b a c => {|a} -> {|b} -> {|c}
+foreign import merge :: forall a b u c. RecordUnion b a u c => {|a} -> {|b} -> {|c}

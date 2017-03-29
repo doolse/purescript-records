be9b042fccf87c9e5c307923c16db0ebd032ca19
Jolse Maginnis <doolse@gmail.com>
2017-03-30 00:26:33 +1100

    Fill in some docs

--- src/Data/Record.purs
+++ src/Data/Record.purs
@@ -2,7 +2,8 @@ module Data.Record (
   merge,
   mergeDefaults,
   class Subrow,
-  class MergeDefaults
+  class MergeDefaults,
+  class IntersectRow
 ) where
 
 -- | Merge two records together unsafely.
@@ -19,14 +20,25 @@ merge = unsafeMerge
 
 -- | Proof that row `r` is a subset of row `s`
 class Subrow (r :: # Type) (s :: # Type)
-instance subrow :: Union r t s => Subrow r s
+instance srInst :: Union r t s => Subrow r s
+
+-- | Proof of row `i` being the intersection of rows `ri` and `si`,
+-- | `r` is `i` subtracted from `ri` and
+-- | `s` is `i` subtracted from `si`
+class IntersectRow (ri :: # Type) (si :: # Type) (i :: # Type) (r :: # Type) (s :: # Type)
+instance irInst :: (Union r i ri, Union i s si) => IntersectRow ri si i r s
 
 class MergeDefaults (o :: # Type) (mr :: # Type) (mo :: # Type)
-instance mergeInstance :: (Union o m mo, Union m r mr, Subrow r o) => MergeDefaults o mr mo
+instance mdInst :: (IntersectRow mo mr m o r, Subrow r o) => MergeDefaults o mr mo
 
--- | Merge a record `mr` with optional default values `o`.
+-- | Merge a record `mr` with optional default values `o`, resulting in record `mo`.
 -- |
 -- | The record `mr` must consist of the common fields from `mo` and `mr` plus a subset
--- | of fields from `o`. The result is the closed row `mo`.
+-- | of fields from `o`.
+-- |
+-- | Examples:
+-- | * mergeDefaults {a:1,b:"Unspecified"} {a:3,c:"Mandatory"} = {a:3,b:"Unspecified",c:"Mandatory"}
+-- | * mergeDefaults {a:1,b:"Unspecified"} {c:"Only mandatory"} = {a:1,b:"Unspecified",c:"Only Mandatory"}
+-- | * mergeDefaults {a:1,b:"Unspecified"} {a:"Wrong type"} = wont compile
 mergeDefaults :: forall o mr mo. MergeDefaults o mr mo => Record o -> Record mr -> Record mo
 mergeDefaults = unsafeMerge
